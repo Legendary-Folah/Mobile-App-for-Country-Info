3eb9eb3b31c6a43fc366319908de406ee18bbccb
FolaDev <folarinfadeyi@gmail.com>
2025-02-10 16:34:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mobile_app_for_country_info_with_theme_customization
+
+A new Flutter project.
+# Mobile-App-for-Country-Info
---
+++ android/app/src/main/kotlin/com/example/mobile_app_for_country_info_with_theme_customization/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobile_app_for_country_info_with_theme_customization
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
